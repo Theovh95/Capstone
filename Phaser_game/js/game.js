20a5638ac2316153f482aa9944d29998db4048cb
Theo Van Houten <theovh95@gmail.com>
2018-04-09 17:56:48 -0500

    added simple css and a phaser canvas

---
+++ Phaser_game/js/game.js
@@ -0,0 +1,19 @@
+var game = new Phaser.Game(900, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
+
+function preload() {
+
+}
+
+function create() {
+  
+
+}
+
+function update() {
+	
+
+}
+
+
+function render() {
+}
