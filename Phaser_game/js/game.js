8525b2571fbb4a34f1f29ae3886ec152b60fd9d5
Theo Van Houten <theovh95@gmail.com>
2018-04-15 22:19:18 -0500

    updated phaser game along with extra possible word js files to make a text adventure.

--- Phaser_game/js/game.js
+++ Phaser_game/js/game.js
@@ -1,12 +1,21 @@
-var game = new Phaser.Game(900, 600, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
+var game = new Phaser.Game(900, 320, Phaser.CANVAS, 'game', { preload: preload, create: create, update: update, render: render });
 
 function preload() {
 
+	game.load.image('background', 'images/forest2.png');
+    game.load.image('hero', 'images/hero.gif');
+    game.load.image('ground', 'images/ground.png');
 }
 
 function create() {
   
+background = game.add.tileSprite(0, 0, game.width, game.height, 'background');
+background.scale.setTo(1,2);
+	
+hero = game.add.sprite(0,260, 'hero');
 
+ground = game.add.tileSprite(0,300, game.width, 25, 'ground');
+	
 }
 
 function update() {
